da5c392fa342dc0610a48f814c24d293960b6c2e
Joakim Annebäck <joakim.anneback@gmail.com>
2016-05-20 13:22:21 +0200

    Updated UI, added code for testing

--- www/js/fileControllers.js
+++ www/js/fileControllers.js
@@ -1,32 +1,41 @@
 (function(){
 	var module = angular.module('file-controllers', ['ngCordova']);
 	module.controller('FileCtrl',['$scope', '$cordovaFile' ,function($scope, $cordovaFile){
-	$scope.output = '';
-	var dataTestFile = $.get('files/test1mb.txt'); // 1mb testfile (test1mb) or 512kb testfile(test512kb)
+		var output = "test.txt";
+		$scope.output = output;
+		$scope.inputfile="files/test512kb.txt";
+		var dataTestFile = $.get($scope.inputfile); // 1mb testfile (test1mb) or 512kb testfile(test512kb)
+		$scope.Wtime = 0; //Write measure
+		$scope.Rtime = 0;// read measure
 		document.addEventListener('deviceready', function(){
-			
+
 			$scope.writeTestFile = function() {
-			var wTFStart = new Date().getTime(); //timestamp for start of writing file
-			console.log('writeFile started at ' + wTFStart);
-				$cordovaFile.writeFile(cordova.file.dataDirectory , "test.txt", dataTestFile, true).then(function(success){
-				//console.log("Wrote " + dataTestFile + " to test.txt");
-				}, function(err){	
+				var start = new Date().getTime(); //timestamp for start of writing file
+				console.log('writeFile started at ');
+				$cordovaFile.writeFile(cordova.file.dataDirectory , output, dataTestFile, true)
+				.then(function(success){
+					var end = new Date().getTime();
+					$scope.Wtime = end - start;
+					//console.log("Wrote " + dataTestFile + " to test.txt");
+				}, function(err){
 					console.log('write error '+err.code)
 				});
 			};
-			
-			$scope.readTestFile = function(){ 
-			var rTFStart = new Date().getTime(); //timestamp for start reading of file
-			console.log('readFile started at '+rTFStart);
-				$cordovaFile.readAsText(cordova.file.dataDirectory, 'test.txt')
+
+			$scope.readTestFile = function(){
+				var start = new Date().getTime(); //timestamp for start reading of file
+				console.log('readFile started at ');
+				$cordovaFile.readAsText(cordova.file.dataDirectory, output)
 				.then(function (success) {
 					//console.log(success);
-					$scope.output = JSON.parse(success).responseText;
+					//$scope.output = JSON.parse(success).responseText;
+					var end = new Date().getTime();
+					$scope.Rtime = end - start;
 				}, function (err) {
 					console.log('Read error '+err.code);
 				});
 			};
 		});
 	}]);
-	
+
 })();
--- www/js/services/fileService.js
+++
@@ -1,8 +0,0 @@
-(function(){
-
-var module = angular.module('file-service',['ngCordova']);
-
-  module.service('FileService',[ '$cordovaFile', function ($cordovaFile) {
-  }]);
-
-})();
